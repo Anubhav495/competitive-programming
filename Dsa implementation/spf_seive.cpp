29caf37ad89c9f7b130b3d759cf7cbf0454d2e21
Anubhav495 <67906361+Anubhav495@users.noreply.github.com>
2021-01-06 11:20:06 +0530

    Create spf_seive.cpp

---
+++ Dsa implementation/spf_seive.cpp
@@ -0,0 +1,46 @@
+#define MAXN   1000002 
+  
+// stores smallest prime factor for every number 
+int spf[MAXN]; 
+  
+// Calculating SPF (Smallest Prime Factor) for every 
+// number till MAXN. 
+// Time Complexity : O(nloglogn) 
+void sieve() 
+{ 
+    spf[1] = 1; 
+    for (int i=2; i<MAXN; i++) 
+  
+        // marking smallest prime factor for every 
+        // number to be itself. 
+        spf[i] = i; 
+  
+    // separately marking spf for every even 
+    // number as 2 
+    for (int i=4; i<MAXN; i+=2) 
+        spf[i] = 2; 
+  
+    for (int i=3; i*i<MAXN; i++) 
+    { 
+        // checking if i is prime 
+        if (spf[i] == i) 
+        { 
+            // marking SPF for all numbers divisible by i 
+            for (int j=i*i; j<MAXN; j+=i) 
+  
+                // marking spf[j] if it is not  
+                // previously marked 
+                if (spf[j]==j) 
+                    spf[j] = i; 
+        } 
+    } 
+}
+
+//to factorize
+int temp=a[i];
+map<int,int> tempo;
+while(temp!=1) 
+{ 
+  tempo[spf[temp]]++;
+    temp = temp/spf[temp];
+}
