d629fff4a0486cc6050178f279e2442f28b2502a
Anubhav495 <67906361+Anubhav495@users.noreply.github.com>
2021-01-07 20:54:15 +0530

    Update modnum.cpp

--- files/modnum.cpp
+++ files/modnum.cpp
@@ -1,43 +1,129 @@
-const int mod=1e9+7;
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define FAST  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+//#define int long long 
+//#define ll long long
+#define all(a) a.begin(),a.end()	
+#define rev(a) a.rbegin(),a.rend()	
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; //less_equal for multiset
+#define ar array
+#define pb push_back
+#define fi(a,b) for(int i=a;i<(b);i++)
+#define fj(a,b) for(int j=a;j<(b);j++)
+#define fk(a,b) for(int k=a;k<(b);k++)
+const double pi=acosl(-1);
 
-int binpow(int a,int b)
-{
-	int res=1;
-	while(b)
-	{
-		if(b&1)
-		{
-			res=(res*a)%mod;
-		}
-		a=(a*a)%mod;
-		b=b>>1;
+
+
+template <int MOD_> struct modnum {
+	static constexpr int MOD = MOD_;
+	static_assert(MOD_ > 0, "MOD must be positive");
+ 
+private:
+	using ll = long long;
+ 
+	int v;
+ 
+	static int minv(int a, int m) {
+		a %= m;
+		assert(a);
+		return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
 	}
-	return res%mod;
-}
-
-int add(int a,int b)
-{
-	return (a+b)%mod;
-}
-
-int mul(int a,int b)
-{
-	return (a*b)%mod;
-}
-
-int sub(int a,int b)
-{
-	int res=a-b;
-	if(res<0)
-	{
-		res+=mod;
+ 
+public:
+ 
+	modnum() : v(0) {}
+	modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+	explicit operator int() const { return v; }
+	friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+	friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+ 
+	friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+	friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+ 
+	modnum inv() const {
+		modnum res;
+		res.v = minv(v, MOD);
+		return res;
 	}
-	res%=mod;
-	return res;
+	friend modnum inv(const modnum& m) { return m.inv(); }
+	modnum neg() const {
+		modnum res;
+		res.v = v ? MOD-v : 0;
+		return res;
+	}
+	friend modnum neg(const modnum& m) { return m.neg(); }
+ 
+	modnum operator- () const {
+		return neg();
+	}
+	modnum operator+ () const {
+		return modnum(*this);
+	}
+ 
+	modnum& operator ++ () {
+		v ++;
+		if (v == MOD) v = 0;
+		return *this;
+	}
+	modnum& operator -- () {
+		if (v == 0) v = MOD;
+		v --;
+		return *this;
+	}
+	modnum& operator += (const modnum& o) {
+		v += o.v;
+		if (v >= MOD) v -= MOD;
+		return *this;
+	}
+	modnum& operator -= (const modnum& o) {
+		v -= o.v;
+		if (v < 0) v += MOD;
+		return *this;
+	}
+	modnum& operator *= (const modnum& o) {
+		v = int(ll(v) * ll(o.v) % MOD);
+		return *this;
+	}
+	modnum& operator /= (const modnum& o) {
+		return *this *= o.inv();
+	}
+ 
+	friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+	friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+	friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+	friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+	friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+	friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+};
+
+void solve()
+{
+	#warning int_is_not_equal_to_Long_Long
+	
+	
+	
+	
+}	
+
+signed main()
+{
+	FAST;
+	int tt=1;
+	cin>>tt;	
+	while(tt--)
+	{
+		solve();
+	}	
 }
 
-int div(int a,int b)
-{
-	int inv_b=binpow(b,mod-2);
-	return (a*inv_b)%mod;
-}
+
+
